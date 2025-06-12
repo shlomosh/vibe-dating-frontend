4711c17b9612b5d672c3c01189e43ce9af370468
Shlomo Shachar <shlomo.shachar@binatix-labs.com>
2025-06-12 15:19:41 +0300

    Rename application to VIBE.

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import mkcert from 'vite-plugin-mkcert';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/cochi/',
+  base: '/vibe/',
   css: {
     preprocessorOptions: {
       scss: {
